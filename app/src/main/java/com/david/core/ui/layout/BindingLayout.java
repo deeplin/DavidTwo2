dd7dcc40198268e6d9cd7ae1e1d997ce489662ab
deeplin <10525677@qq.com>
2020-10-31 21:25:02 +0800

    update

---
+++ app/src/main/java/com/david/core/ui/layout/BindingLayout.java
@@ -0,0 +1,39 @@
+package com.david.core.ui.layout;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.lifecycle.LifecycleOwner;
+
+import com.david.core.enumeration.LayoutPageEnum;
+import com.david.core.util.ILifeCycleOwner;
+
+public abstract class BindingLayout<U extends ViewDataBinding> extends PopupLayout implements ILifeCycleOwner {
+
+    protected final U binding;
+
+    public BindingLayout(Context context, LayoutPageEnum layoutPageEnum) {
+        super(context, layoutPageEnum);
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        binding = DataBindingUtil.inflate(inflater, layoutPageEnum.getLayoutId(), this, true);
+    }
+
+    @Override
+    public void attach(LifecycleOwner lifecycleOwner) {
+        super.attach();
+        binding.setLifecycleOwner(lifecycleOwner);
+    }
+
+    @Override
+    public void detach() {
+        super.detach();
+        binding.setLifecycleOwner(null);
+    }
+
+//    @Override
+//    protected ConstraintLayout getRoot() {
+//        return (ConstraintLayout) binding.getRoot();
+//    }
+}
