dfbe564dbec5f0523237239dfd767ccfd76097c4
deeplin <10525677@qq.com>
2020-11-01 11:13:31 +0800

    update

--- app/src/main/java/com/david/incubator/ui/main/PopupLayout.java
+++ app/src/main/java/com/david/incubator/ui/main/PopupLayout.java
@@ -6,6 +6,7 @@ import android.view.View;
 import androidx.lifecycle.LifecycleOwner;
 
 import com.david.R;
+import com.david.core.enumeration.LayoutPageEnum;
 import com.david.core.model.SystemModel;
 import com.david.core.ui.layout.BindingBasicLayout;
 import com.david.core.util.ContextUtil;
@@ -20,11 +21,14 @@ public class PopupLayout extends BindingBasicLayout<LayoutPopupBinding> {
     SystemModel systemModel;
 
     private ILifeCycleOwner currentLayout;
-//    private final ILifeCycleOwner[] views = new ILifeCycleOwner[LayoutPageEnum.LAYOUT_ALL.ordinal()];
+    private final ILifeCycleOwner[] views =
+            new ILifeCycleOwner[LayoutPageEnum.LAYOUT_NONE.ordinal() - LayoutPageEnum.MENU_HOME.ordinal()];
 
     public PopupLayout(Context context) {
         super(context);
         ContextUtil.getComponent().inject(this);
+
+//        views[LayoutPageEnum.SWITCH_SCREEN.ordinal()] = new Swi
     }
 
     @Override
@@ -36,6 +40,7 @@ public class PopupLayout extends BindingBasicLayout<LayoutPopupBinding> {
     public void attach(LifecycleOwner lifeCycleOwner) {
         super.attach(lifeCycleOwner);
 //        binding.incubatorVerticalListLayout.attach(lifeCycleOwner);
+        currentLayout = views[systemModel.layoutPage.getValue().ordinal() - LayoutPageEnum.MENU_HOME.ordinal()];
         currentLayout.attach(lifeCycleOwner);
         binding.mainFrameLayout.addView((View) currentLayout);
     }
