5efe60e0ca463625115871e15d249b3f547ebfd5
deeplin <10525677@qq.com>
2020-11-03 21:00:16 +0800

    update

--- app/src/main/java/com/david/incubator/ui/main/IncubatorListLayout.java
+++ app/src/main/java/com/david/incubator/ui/main/IncubatorListLayout.java
@@ -8,7 +8,9 @@ import androidx.lifecycle.LifecycleOwner;
 import com.david.R;
 import com.david.core.control.ModuleHardware;
 import com.david.core.control.SensorModelRepository;
+import com.david.core.enumeration.SensorModelEnum;
 import com.david.core.model.IncubatorModel;
+import com.david.core.model.SensorModel;
 import com.david.core.ui.layout.BindingBasicLayout;
 import com.david.core.util.ContextUtil;
 import com.david.databinding.LayoutIncubatorListBinding;
@@ -27,9 +29,13 @@ public class IncubatorListLayout extends BindingBasicLayout<LayoutIncubatorListB
     public IncubatorListLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
         ContextUtil.getComponent().inject(this);
-//        binding.skin1SensorView.setSmallLayout();
-//        binding.skin2SensorView.setSmallLayout();
-//        binding.skin2SensorView.setIncubatorVertical();
+
+        SensorModel skin1SensorModel = sensorModelRepository.getSensorModel(SensorModelEnum.Skin1);
+        binding.skin1SensorView.set(skin1SensorModel);
+        binding.skin1SensorView.setSensorDarkMode(false);
+        SensorModel skin2SensorModel = sensorModelRepository.getSensorModel(SensorModelEnum.Skin2);
+        binding.skin2SensorView.set(skin2SensorModel);
+        binding.skin2SensorView.setSensorDarkMode(false);
     }
 
     @Override
@@ -40,13 +46,7 @@ public class IncubatorListLayout extends BindingBasicLayout<LayoutIncubatorListB
     @Override
     public void attach(LifecycleOwner lifecycleOwner) {
         super.attach(lifecycleOwner);
-//        ThemeSensorModel bodySensorModel = themeSensorRepository.getThemeSensorModel(ThemeSensorEnum.Body);
-//        ThemeSensorModel machineSensorModel = themeSensorRepository.getThemeSensorModel(ThemeSensorEnum.Machine);
-//
-//        SensorModel skin1SensorModel = sensorModelRepository.getSensorModel(SensorModelEnum.Skin1);
-//        binding.skin1SensorView.set(skin1SensorModel, bodySensorModel);
-//
-//        binding.homeSetView.attach(lifecycleOwner);
+        binding.homeSetView.attach(lifecycleOwner);
 //
 //        if (incubatorModel.isCabin()) {
 //            SensorModel airSensorModel = sensorModelRepository.getSensorModel(SensorModelEnum.Air);
@@ -76,8 +76,8 @@ public class IncubatorListLayout extends BindingBasicLayout<LayoutIncubatorListB
 
     @Override
     public void detach() {
-//        super.detach();
-//        binding.homeSetView.detach();
+        super.detach();
+        binding.homeSetView.detach();
     }
 
 //    private void setInnerView(IncubatorListView incubatorListView, SensorModelEnum sensorModelEnum,
